842e07870c6dbb8501fd4820d6e37b99abdbb9d5
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-04-13 15:40:16 +0200

    Java version of DataTypes example added

---
+++ src/javakotlinexamples/java/DataTypes.java
@@ -0,0 +1,26 @@
+package javakotlinexamples.java;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        float a = 123.45f;
+        double b = 123.45;
+        long c = 1;
+        int d = 2;
+        short e = 3;
+        byte f = 4;
+        String g = "Hallo";
+
+        print(a);
+        print(b);
+        print(c);
+        print(d);
+        print(e);
+        print(f);
+        print(g);
+    }
+
+    private static void print(Object n) {
+        System.out.println(n.getClass().getName());
+    }
+}
