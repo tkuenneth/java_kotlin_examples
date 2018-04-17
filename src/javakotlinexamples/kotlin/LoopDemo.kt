8f78427d6f645e6269704bdb7badd20148509cb2
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-04-17 18:50:01 +0200

    LoopDemo added

---
+++ src/javakotlinexamples/kotlin/LoopDemo.kt
@@ -0,0 +1,16 @@
+package javakotlinexamples.kotlin
+
+fun main(args: Array<String>) {
+    for (i in 1..3) {
+        println(i)
+    }
+
+    for (i in 6 downTo 0 step 2) {
+        println(i)
+    }
+
+    val array = arrayOf("Eins", "Zwei", "Drie")
+    for (i in array.indices) {
+        println(array[i])
+    }
+}
