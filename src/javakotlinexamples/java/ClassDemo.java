1fc2eb10e5d3efc59d7ddc3634ecc2d04072f3d2
Thomas Kuenneth <kuennetht@gmail.com>
2020-01-02 07:48:33 +0100

    Simplified and improved example

--- src/javakotlinexamples/java/ClassDemo.java
+++ src/javakotlinexamples/java/ClassDemo.java
@@ -3,21 +3,15 @@ package javakotlinexamples.java;
 public class ClassDemo {
 
     public static void main(String[] args) {
-        POJO pojo = new POJO("Hallo", 1);
-        pojo.setaString("MATHEMA");
-        pojo.setAnInt(42);
-        print(pojo);
-
-        pojo = new POJO(false);
-        print(pojo);
-
-        pojo = new POJO(true);
-        print(pojo);
+        Person person = new Person("John", 50);
+        person.setName("Max");
+        person.setAge(42);
+        print(person);
     }
 
-    private static void print(POJO pojo) {
+    private static void print(Person person) {
         System.out.println(String.format("%s, %d",
-                pojo.getaString(),
-                pojo.getComputedValue()));
+                person.getName(),
+                person.getAge()));
     }
 }
--- src/javakotlinexamples/kotlin/ClassDemo.kt
+++ src/javakotlinexamples/kotlin/ClassDemo.kt
@@ -1,37 +1,15 @@
 package javakotlinexamples.kotlin
 
 fun main(args: Array<String>) {
-    var poko = POKO("Hallo", 1)
-    poko.aString = "MATHEMA"
-    poko.anInt = 42
-    print(poko)
-
-    poko = POKO(false)
-    print(poko)
-
-    poko = POKO(true)
-    print(poko)
+    val person = Person("John", 50)
+    person.name = "Max"
+    person.age = 42
+    print(person)
 }
 
-fun print(poko: POKO) {
-    println("${poko.aString}, ${poko.computedValue}")
+fun print(person: Person) {
+    println("${person.name}, ${person.age}")
 }
 
-class POKO constructor(var aString: String, var anInt: Int) {
-
-    companion object {
-        var counter: Int = 0
-    }
-
-    val computedValue: Int
-        get() = anInt * anInt
-
-    init {
-        println(POKO.counter++)
-    }
-
-    constructor(aBoolean: Boolean) : this(
-            if (aBoolean) "MATHEMA" else "Campus,",
-            if (aBoolean) 3 else 4
-    )
+class Person(var name: String, var age: Int) {
 }
